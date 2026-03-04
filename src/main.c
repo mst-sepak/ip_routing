92d6e9a930e6f2439a75ab8193ee01f63ea473e8
masato-hasegawa <hmasato0616@gmail.com>
2026-03-04 04:47:36 +0000

    add sample files

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "router.h"
+
+int main(void) {
+    router_run();
+    return 0;
+}
---
+++ src/router.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "router.h"
+
+void router_run(void) {
+    printf("simple ip router (stub)\n");
+}
