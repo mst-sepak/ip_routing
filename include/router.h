e429906e6b63e03c6914d330f2d6bc2fb5cac98c
masato-hasegawa <hmasato0616@gmail.com>
2026-03-04 08:20:44 +0000

    delete router.c/.h

--- include/router.h
+++
@@ -1,6 +0,0 @@
-#ifndef ROUTER_H
-#define ROUTER_H
-
-void router_run(void);
-
-#endif
--- src/router.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "router.h"
-
-void router_run(void) {
-    printf("simple ip router (stub)\n");
-}
